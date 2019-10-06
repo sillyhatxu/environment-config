0438e97ac67eea0256325cada173c063e7830f8e
sillyhat <xushikuan1@163.com>
2019-10-06 20:59:37 +0800

    add remind db config

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git add .
-git commit -m 'support os.env'
+git commit -m 'add remind db config'
 git push origin master
-git tag v1.0.7
-git push origin v1.0.7
+git tag v1.0.8
+git push origin v1.0.8
